b581c417f78b60834a26fa9067e5230a4e6e9bfe
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-04-24 16:18:02 -0300

    feat: :sparkles: FilterOddNumbers solved.

--- FilterOddNumbers/src/FilterOddNumbers.sol
+++ FilterOddNumbers/src/FilterOddNumbers.sol
@@ -10,11 +10,27 @@ contract FilterOddNumbers {
               you need to count the even numbers then declare an array of that size.
     */
 
-    function filterOdd(uint256[] memory _arr)
-        public
-        view
-        returns (uint256[] memory)
-    {
-        // your code here
+    function filterOdd(
+        uint256[] memory _arr
+    ) public view returns (uint256[] memory) {
+        uint256 evenCount = 0;
+        uint256[] memory evensArray;
+
+        for (uint i = 0; i < _arr.length; i++) {
+            if (_arr[i] % 2 == 0) {
+                evenCount++;
+            }
+        }
+
+        evensArray = new uint256[](evenCount);
+        uint256 index = 0;
+
+        for (uint i = 0; i < _arr.length; i++) {
+            if (_arr[i] % 2 == 0) {
+                evensArray[index] = _arr[i];
+                index++;
+            }
+        }
+        return evensArray;
     }
 }
