dbbb4288dfa92609e179e3422e7d7841d5a9a57e
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-04-25 08:49:58 -0300

    feat: :sparkles: TicTacToe solved.

--- TicTacToe/src/TicTacToe.sol
+++ TicTacToe/src/TicTacToe.sol
@@ -11,6 +11,69 @@ contract TicTacToe {
     */
 
     function isWinning(uint8[3][3] memory board) public pure returns (bool) {
-        // your code here
+        uint256 sumRowOne = 0;
+        uint256 sumRowTwo = 0;
+        uint256 sumRowThree = 0;
+        uint256 sumColOne = 0;
+        uint256 sumColTwo = 0;
+        uint256 sumColThree = 0;
+        uint256 sumTopLeftToBotRight = 0;
+        uint256 sumBotLeftToTopRight = 0;
+
+        for (uint256 i = 0; i < board.length; i++) {
+            for (uint256 j = 0; j < board[i].length; j++) {
+                if (i == 0) {
+                    sumRowOne += board[i][j];
+                }
+
+                if (i == 1) {
+                    sumRowTwo += board[i][j];
+                }
+
+                if (i == 2) {
+                    sumRowThree += board[i][j];
+                }
+
+                if (j == 0) {
+                    sumColOne += board[i][j];
+                }
+
+                if (j == 1) {
+                    sumColTwo += board[i][j];
+                }
+
+                if (j == 2) {
+                    sumColThree += board[i][j];
+                }
+
+                if (i == 1 && j == 1) {
+                    sumTopLeftToBotRight += board[i][j];
+                    sumBotLeftToTopRight += board[i][j];
+                }
+
+                if (i != 1 && i == j) {
+                    sumTopLeftToBotRight += board[i][j];
+                }
+
+                if ((i == 0 && j == 2) || (i == 2 && j == 0)) {
+                    sumBotLeftToTopRight += board[i][j];
+                }
+            }
+        }
+
+        if (
+            sumRowOne == 3 ||
+            sumRowTwo == 3 ||
+            sumRowThree == 3 ||
+            sumColOne == 3 ||
+            sumColTwo == 3 ||
+            sumColThree == 3 ||
+            sumBotLeftToTopRight == 3 ||
+            sumTopLeftToBotRight == 3
+        ) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
