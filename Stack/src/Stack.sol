a2e34038990b1e9435be953aef151b8b1d40f5a7
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-04-25 10:28:23 -0300

    feat: :sparkles: Stack solved.

--- Stack/src/Stack.sol
+++ Stack/src/Stack.sol
@@ -21,5 +21,33 @@ contract Stack {
         stack = _stack;
     }
 
-    // your code here
+    function push(uint256 _newValue) public {
+        stack.push(_newValue);
+    }
+
+    function peek() public view returns (uint256) {
+        if (stack.length > 0) {
+            return stack[stack.length - 1];
+        }
+
+        revert();
+    }
+
+    function pop() public returns (uint256) {
+        if (stack.length > 0) {
+            uint256 lastElement = stack[stack.length - 1];
+            stack.pop();
+            return lastElement;
+        }
+
+        revert();
+    }
+
+    function size() public view returns (uint256) {
+        return stack.length;
+    }
+
+    function getStack() public view returns (uint256[] memory) {
+        return stack;
+    }
 }
